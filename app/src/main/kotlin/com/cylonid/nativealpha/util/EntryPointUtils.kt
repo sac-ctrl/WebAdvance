af82571a463df3fce682bb10ade1e6baf1d42024
Hannes Unterweger <h.unterweger@student.tugraz.at>
2025-05-18 15:55:02 +0200

    Increase news trigger difference

--- app/src/main/kotlin/com/cylonid/nativealpha/util/EntryPointUtils.kt
+++ app/src/main/kotlin/com/cylonid/nativealpha/util/EntryPointUtils.kt
@@ -9,7 +9,7 @@ import com.cylonid.nativealpha.activities.NewsActivity
 object EntryPointUtils {
     @JvmStatic
     fun entryPointReached(a: Activity) {
-        if (kotlin.math.abs(DataManager.getInstance().lastShownUpdate - BuildConfig.VERSION_CODE) > 10 ) {
+        if (kotlin.math.abs(DataManager.getInstance().lastShownUpdate - BuildConfig.VERSION_CODE) > 50 ) {
             a.startActivity(Intent(a, NewsActivity::class.java))
         }
         DataManager.getInstance().loadAppData()
